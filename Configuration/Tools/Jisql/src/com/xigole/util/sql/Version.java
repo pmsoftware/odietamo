c48b6fe96989a91565ee9a0e4bc37ea9148a2545
Mark Matten <mark_matten@hotmail.com>
2013-01-18 22:34:25 +0000

    Initial rebuild

---
+++ Configuration/Tools/Jisql/src/com/xigole/util/sql/Version.java
@@ -0,0 +1,6 @@
+package com.xigole.util.sql;
+/* this class is auto-generated during an ant build */
+public class Version {
+    public static final String version = "2.0.11.443";
+    public static final String buildDate = "Sep 07, 2011 at 09:46:07 AM MDT";
+}
