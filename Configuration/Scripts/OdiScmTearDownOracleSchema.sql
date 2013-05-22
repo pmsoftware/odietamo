ead0f1a97f7fc3b1123f6fca259dbc020f834e62
Mark Matten <mark_matten@hotmail.com>
2013-05-22 15:48:37 +0100

    Added dropping of stored procedures during schema tear down

--- Configuration/Scripts/OdiScmTearDownOracleSchema.sql
+++ Configuration/Scripts/OdiScmTearDownOracleSchema.sql
@@ -15,17 +15,31 @@ BEGIN
         END;
     END LOOP;
     
-	FOR c_repo_dbl IN (
-	                  SELECT db_link
-	                    FROM user_db_links
-	                  )
-	LOOP
-		BEGIN
-			EXECUTE IMMEDIATE('DROP DATABASE LINK ' || c_repo_dbl.db_link);
-		EXCEPTION
-			WHEN OTHERS
-				THEN raise_application_error(-20000, 'Cannot drop database link ' || c_repo_dbl.db_link);
-		END;
+    FOR c_repo_dbl IN (
+                      SELECT db_link
+                        FROM user_db_links
+                      )
+    LOOP
+        BEGIN
+            EXECUTE IMMEDIATE('DROP DATABASE LINK ' || c_repo_dbl.db_link);
+        EXCEPTION
+            WHEN OTHERS
+                THEN raise_application_error(-20000, 'Cannot drop database link ' || c_repo_dbl.db_link);
+        END;
+    END LOOP;
+    
+    FOR c_repo_prc IN (
+                      SELECT object_name
+                           , object_type
+                        FROM user_procedures
+                      )
+    LOOP
+        BEGIN
+            EXECUTE IMMEDIATE('DROP ' || c_repo_prc.object_type || ' ' || c_repo_prc.object_name);
+        EXCEPTION
+            WHEN OTHERS
+                THEN raise_application_error(-20000, 'Cannot drop ' || c_repo_prc.object_type || ' ' || c_repo_prc.object_name);
+        END;
     END LOOP;
 END;
 /
